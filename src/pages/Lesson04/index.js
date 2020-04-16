6503cd5e8b9fc02423f2f4483c86e2b6df1e8371
tsubasa <9911tsubasa@gmail.com>
2020-04-16 14:57:54 +0900

    04 fin?

--- src/pages/Lesson04/index.js
+++ src/pages/Lesson04/index.js
@@ -4,7 +4,18 @@ import Chart from "../../components/Chart04";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  const species = Array.from(new Set(input.map(({species}) => species)));
+  //inputのspeciesをspecies:○○という形で重複なく配列にするためSetとArray.form
+  return species.map((species) =>{//出来た配列をmapしていく
+    return{
+      id: species,//花の名前
+      data: input
+        .filter((item) => item.species === species)
+        //inputの花の名前が配列speciesと同じのをfilter
+        .map(({sepalLength: x, petalWidth: y })=>({x,y})),
+        //その要素のsepalLegthをx,同様にyとする。そしてそれを{x,y}としてオブジェクトに
+      };
+     }); // ここを作りましょう！
 };
 
 const Lesson = () => {
